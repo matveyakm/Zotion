1a1e0458b3941ec515c955ac3f9a3937433b9609
matveyakm <matveyakm@gmail.com>
2025-07-24 15:23:27 +0700

    fix: remove call to undefined function

--- src/content.ts
+++ src/content.ts
@@ -1,17 +1,9 @@
+// content.ts
+
 import { injectStyles } from './modules/textFormatting/constants';
 import { processTextStyleLinks } from './modules/textFormatting/scanner';
 import { setupMutationObserver } from './modules/textFormatting/init';
 
 injectStyles();
-
-document.addEventListener('DOMContentLoaded', () => {
-  setupStyleSelection();
-});
-
-setTimeout(() => {
-  if (!document.getElementById('custom-style-panel')) {
-    setupStyleSelection();
-  }
-}, 3000);
 
 setupMutationObserver();
