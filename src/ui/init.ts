770c70a4c846e4a3a2aceaf4dc3cac7a4af8da84
matveyakm <matveyakm@gmail.com>
2026-03-06 19:56:08 +0300

    fix: show edit panel only for page editors

--- src/ui/init.ts
+++ src/ui/init.ts
@@ -2,6 +2,12 @@ import { initTextPanel } from "./textPanel/textPanel";
 import { initMainPanel } from "./mainPanel/mainPanel";
 
 export function initUI() {
-    initMainPanel();
-    initTextPanel();
+    // Показывает панель редактирования, только если пользователь -- редактор страницы
+    const interval = setInterval(() => {
+        if (document.querySelector('.notion-sidebar-switcher') !== null) {
+            initMainPanel();
+            initTextPanel();
+            clearInterval(interval);
+        }
+    }, 5000);
 }
