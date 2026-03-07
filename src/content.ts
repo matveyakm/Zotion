3e3d26c60a49b6e48327290e34b3c8a2e80ef96b
matveyakm <matveyakm@gmail.com>
2026-03-08 01:56:03 +0300

    feat(ui): add timer main panel

--- src/content.ts
+++ src/content.ts
@@ -4,13 +4,11 @@ import { injectStyles } from './modules/DOMModifier/constants';
 import { setupMutationObserver } from './modules/DOMModifier/init';
 import { initUI } from './ui/init';
 import { initExamMode } from './modules/examMode/examMode';
-import { initExamModePanel } from './ui/examModePanel/examModePanel';
 
 
 injectStyles();
 setupMutationObserver();
 
-initUI();
-
 initExamMode();
-initExamModePanel();
+
+initUI();
--- src/ui/init.ts
+++ src/ui/init.ts
@@ -1,12 +1,17 @@
 import { initTextPanel } from "./textPanel/textPanel";
 import { initMainPanel } from "./mainPanel/mainPanel";
+import { initExamModePanel } from './examModePanel/examModePanel';
+import { initBlockPanel } from "./blockPanel/blockPanel";
 
 export function initUI() {
+    initExamModePanel();
+
     // Показывает панель редактирования, только если пользователь -- редактор страницы
     const interval = setInterval(() => {
         if (document.querySelector('.notion-sidebar-switcher') !== null) {
             initMainPanel();
             initTextPanel();
+            initBlockPanel();
             clearInterval(interval);
         }
     }, 5000);
